d08a61d48652ed6150080b716736cb0a45a7cdf3
avatarkorraa <e.deluca20@studenti.unisa.it>
2024-06-18 14:03:16 +0200

    Modifica index.html, Aggiunta risorse varie, Aggiunta home-utente.css.
    Co-authored-by: AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
    Co-authored-by: Gi0NZ <g.nigro32@studenti.unisa.it>
    Co-authored-by: avatarkorraa <e.deluca20@studenti.unisa.it>

---
+++ src/it.unisa.educae/js/main.js
@@ -0,0 +1,23 @@
+function back(){
+
+    window.location.href = "../html/index.html";
+
+}
+
+function testSection(){
+
+    window.location.href = "../html/test.html";
+
+}
+
+function medals(){
+
+    window.location.href = "../html/bacheca-trofei.html";
+
+}
+
+function exit(){
+
+    window.location.href = "../html/auth-home.html";
+
+}
