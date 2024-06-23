7f719838d84a666277c7783f184ef5de911f8786
avatarkorraa <e.deluca20@studenti.unisa.it>
2024-06-23 11:50:43 +0200

    Modifica index.html, Modifica test-autovalutativo.html, Modifica test-effettivo.html, Modifica home-utente.css, Modifica sidebar.css, Modifica test_autovalutativo.css, Modifica test_effettivo.css, Aggiunta risorse varie, Modifica test-effettivo.html, Modifica test-autovalutativo.html, Modifica risultati-test.html, Modifica risultati-test.css.
    Co-authored-by: AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
    Co-authored-by: Gi0NZ <g.nigro32@studenti.unisa.it>

--- src/it.unisa.educae/js/test.js
+++ src/it.unisa.educae/js/test.js
@@ -1,3 +1,9 @@
+let correctAnswers = [];
+let givedAnswers = [];
+
+let intervalId;
+let remainingSeconds;
+
 async function read_domande(){
 
     try {
@@ -35,7 +41,11 @@ function store_domande(){
 
     read_domande().then(result => {
 
-        localStorage.setItem("domande", JSON.stringify(result));
+        let domandeJSON = JSON.stringify(result);
+
+        localStorage.setItem("domande", domandeJSON);
+        givedAnswers = Array(JSON.parse(domandeJSON).length).fill("0");
+
         domande();
 
     })
@@ -45,6 +55,7 @@ function store_domande(){
 function domande(){
 
     let key = parseInt(document.getElementById("count").innerText) - 1;
+
     let domande = JSON.parse(localStorage.getItem("domande"));
     let questionsDiv = document.getElementById("questions");
 
@@ -59,6 +70,14 @@ function domande(){
     answerYellow.innerText = domande[key].Risposta_due;
     answerBlue.innerText = domande[key].Risposta_tre;
     answerGreen.innerText = domande[key].Risposta_quattro;
+
+    let correctAnswer = domande[key].Risposta_corretta;
+
+    if(!(correctAnswer in correctAnswers)){
+
+        correctAnswers.push(correctAnswer);
+
+    }
 
 }
 
@@ -93,10 +112,164 @@ function change(flag){
 
     }
 
+    for (let el of document.querySelectorAll('.domande')) {
+
+        el.classList.remove('disabled');
+
+    }
+
 }
 
 function finish(){
 
+    localStorage.setItem("domandeCorrette", JSON.stringify(correctAnswers));
+    localStorage.setItem("domandeDate", JSON.stringify(givedAnswers));
+
+    correctAnswers = [];
+    givedAnswers = [];
+
     window.location.href = "../html/risultati-test.html"
 
 }
+
+function startTimer(seconds) {
+
+    remainingSeconds = seconds;
+    const timerElement = document.getElementById('timer');
+    clearInterval(intervalId);
+
+    intervalId = setInterval(() => {
+
+        if (remainingSeconds > 0) {
+
+            const minutes = Math.floor(remainingSeconds / 60);
+            const seconds = remainingSeconds % 60;
+            timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+            remainingSeconds--;
+
+        } else {
+
+            finish();
+
+            clearInterval(intervalId);
+
+        }
+
+    }, 1000);
+
+}
+
+function addTwoMinutes(element) {
+
+    remainingSeconds += 120;
+
+    const timerElement = document.getElementById('timer');
+    const minutes = Math.floor(remainingSeconds / 60);
+    const seconds = remainingSeconds % 60;
+
+    timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+    element.classList.add('disabled');
+
+}
+
+function fifty(elemento){
+
+    let key = parseInt(document.getElementById("count").innerText) - 1;
+    let allDivs = document.querySelectorAll('.answer');
+    let remainingDivs = Array.from(allDivs).filter(div => !(div.innerText === correctAnswers[key]));
+
+    let randomIndex = Math.floor(Math.random() * remainingDivs.length);
+    let randomDiv = remainingDivs[randomIndex];
+
+    for (let el of allDivs) {
+
+        if (el.innerText !== randomDiv.innerText &&
+            el.innerText !== correctAnswers[key]){
+
+            el.parentElement.classList.add('disabled');
+
+        }
+
+    }
+
+    elemento.classList.add('disabled');
+
+}
+
+function giveAnswer(elemento){
+
+    let key = parseInt(document.getElementById("count").innerText) - 1;
+
+    givedAnswers[key] = elemento.children[0].innerText;
+
+    change('avanti');
+
+}
+
+function results() {
+
+    let container = document.getElementById('container');
+
+    let correctAnswers = JSON.parse(localStorage.getItem("domandeCorrette"));
+    let givedAnswers = JSON.parse(localStorage.getItem("domandeDate"));
+
+    container.innerHTML = '';
+
+    let index = 0;
+
+    console.log(correctAnswers);
+
+    for (let correctAnswer of correctAnswers) {
+
+        let questionDiv = document.createElement('div');
+        questionDiv.classList.add('question-result');
+
+
+        let givenAnswer = givedAnswers[index] !== "0" ? givedAnswers[index] : "Non data";
+        let questionText = document.createElement('div');
+
+        questionText.innerText = `Risposta ${index + 1}. ${givenAnswer}`;
+        questionText.classList.add('question-text');
+        questionDiv.appendChild(questionText);
+
+        if (givedAnswers[index] === "0") {
+
+            questionDiv.classList.add('not-answered');
+            questionDiv.classList.add('answerResult');
+
+        } else if (givedAnswers[index] === correctAnswer) {
+
+            questionDiv.classList.add('correct');
+            questionDiv.classList.add('answerResult');
+
+        } else {
+
+            questionDiv.classList.add('incorrect');
+            questionDiv.classList.add('answerResult');
+
+        }
+
+        container.appendChild(questionDiv);
+        index++;
+
+    }
+
+    let correctCount = correctAnswers.filter((ans, idx) => ans === givedAnswers[idx]).length;
+    let incorrectCount = givedAnswers.filter(ans => ans !== "0" && ans !== correctAnswers[givedAnswers.indexOf(ans)]).length;
+    let notAnsweredCount = givedAnswers.filter(ans => ans === "0").length;
+
+    document.getElementById('numero').innerText = correctAnswers.length;
+    document.getElementById('giuste').innerText = correctCount;
+    document.getElementById('wrong').innerText = incorrectCount;
+    document.getElementById('nondate').innerText = notAnsweredCount;
+
+    correctAnswers = [];
+    givedAnswers = [];
+
+    localStorage.setItem("domandeCorrette", JSON.stringify(correctAnswers));
+    localStorage.setItem("domandeDate", JSON.stringify(givedAnswers));
+
+}
+
+
